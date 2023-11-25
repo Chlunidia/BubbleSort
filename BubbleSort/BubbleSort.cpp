3ce73c4c751090a1afeac3754741df5a934d62dd
Chlunidia Putri Kieswendha <kieswendha@gmail.com>
2023-11-25 14:19:21 +0700

    Implement read() method to accept user input for array elements

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -6,5 +6,24 @@ private:
     int n;
 
 public:
+    void read() {
+        while (true) {
+            std::cout << "Enter the number of elements in the array: ";
+            std::cin >> n;
 
+            if (n <= 20)
+                break;
+            else
+                std::cout << "\nArray can have a maximum of 20 elements.\n";
+        }
+
+        std::cout << "\n-----------------------";
+        std::cout << "\nEnter array elements";
+        std::cout << "\n-----------------------\n";
+
+        for (int i = 0; i < n; i++) {
+            std::cout << "<" << (i + 1) << "> ";
+            std::cin >> a[i];
+        }
+    }
 };
